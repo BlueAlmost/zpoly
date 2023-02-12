43afbfc5fa28f9e647aa518865badd090efb1bca
BlueAlmost <git.blue.almost@gmail.com>
2023-02-12 15:58:12 +0100

    aligned with new build process

--- build.zig
+++ build.zig
@@ -1,67 +1,21 @@
 const std = @import("std");
-const Builder = std.build.Builder;
-const Pkg = std.build.Pkg;
-
-const pkgs = struct {
-    const zpoly = Pkg{
-        .name = "zpoly",
-        .source = .{ .path = "./zpoly.zig" },
-        .dependencies = &[_]Pkg {},
-    };
-};
 
 pub fn build(b: *std.build.Builder) void {
+
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe_targets = [_]Target{
-    //    .{ .name = "foo", .src = "example/foo.zig", .desc =""},
-    };
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{.path = "./main_test.zig"},
+        .target = target,
+        .optimize = optimize,
+        });
 
-    for (exe_targets) |e_target| {
-        e_target.build(b);
-    }
-
-    const exe_tests = b.addTest("./main_test.zig");
-
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-
-    exe_tests.addPackage(pkgs.zpoly);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
+    unit_tests.addAnonymousModule("zpoly", .{
+        .source_file = .{.path = "./zpoly.zig"},
+    });
+    
+    const unit_tests_step = b.step("test", "Run unit tests");
+    unit_tests_step.dependOn(&unit_tests.step);
 }
 
-//---------------------------------------------------------------------------
-
-const Target = struct {
-    name: []const u8,
-    src: []const u8,
-    desc: []const u8,
-
-    pub fn build(self: Target, b: *std.build.Builder) void {
-
-        b.exe_dir = b.pathFromRoot("./bin");
-
-        // const target = b.standardTargetOptions(.{});
-        const mode = b.standardReleaseOptions();
-
-        var exe = b.addExecutable(self.name, self.src);
-
-        exe.setBuildMode(b.standardReleaseOptions());
-        // exe.setTarget(target);
-        exe.setBuildMode(mode);
-
-        // zpoly
-        exe.addPackage(pkgs.zpoly);
-
-        exe.install();
-
-        const run_cmd = exe.run();
-        run_cmd.step.dependOn(b.getInstallStep());
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-    }
-};
